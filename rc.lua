18bb335e36dc2b5fb2688b3b99de60eea2ec5744
Bogdan Penkovskyi <masterdezign@gmail.com>
2012-05-01 16:08:28 +0300

    Added tile

--- rc.lua
+++ rc.lua
@@ -30,7 +30,7 @@ modkey = "Mod4"
 layouts =
 {
     awful.layout.suit.floating,
-    --awful.layout.suit.tile,
+    awful.layout.suit.tile,
     --awful.layout.suit.tile.left,
     --awful.layout.suit.tile.bottom,
     --awful.layout.suit.tile.top,
