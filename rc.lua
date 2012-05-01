df24a306bdc25593faa881861f513ae4abb39e20
Bogdan Penkovskyi <masterdezign@gmail.com>
2012-05-01 16:27:09 +0300

    Added comment

--- rc.lua
+++ rc.lua
@@ -351,4 +351,5 @@ client.add_signal("focus", function(c) c.border_color = beautiful.border_focus e
 client.add_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
 
+-- Network manager applet
 os.execute("nm-applet &")
