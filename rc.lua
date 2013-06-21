a2678064ddd174e62100614600167fdb9460f395
Bogdan Penkovskyi <masterdezign@gmail.com>
2013-06-21 10:56:31 +0300

    Add keyboard switcher widget

--- rc.lua
+++ rc.lua
@@ -75,6 +75,25 @@ mylauncher = awful.widget.launcher({ image = image(beautiful.awesome_icon),
                                      menu = mymainmenu })
 -- }}}
 
+-- Keyboard map indicator and changer
+kbdcfg = {}
+kbdcfg.cmd = "setxkbmap"
+kbdcfg.layout = { "us", "ua" }
+kbdcfg.current = 1  -- us is our default layout
+kbdcfg.widget = widget({ type = "textbox", align = "right" })
+kbdcfg.widget.text = " " .. kbdcfg.layout[kbdcfg.current] .. " "
+kbdcfg.switch = function ()
+    kbdcfg.current = kbdcfg.current % #(kbdcfg.layout) + 1
+    local t = " " .. kbdcfg.layout[kbdcfg.current] .. " "
+    kbdcfg.widget.text = t
+    os.execute( kbdcfg.cmd .. t )
+end
+
+-- Mouse bindings
+kbdcfg.widget:buttons(awful.util.table.join(
+awful.button({ }, 1, function () kbdcfg.switch() end)
+))
+
 -- {{{ Wibox
 -- Create a textclock widget
 mytextclock = awful.widget.textclock({ align = "right" })
@@ -158,6 +177,7 @@ for s = 1, screen.count() do
         },
         mylayoutbox[s],
         mytextclock,
+        kbdcfg.widget,
         s == 1 and mysystray or nil,
         mytasklist[s],
         layout = awful.widget.layout.horizontal.rightleft
